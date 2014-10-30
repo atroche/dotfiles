9475be2f2a1bab3c3e386d94e619caba4fdc72a6
Alistair Roche <roche.a@gmail.com>
2014-10-30 01:55:54 +0000

    Make brew installer sensible

--- brew/install.sh
+++ brew/install.sh
@@ -1,18 +1,15 @@
-#! /usr/bin/zsh
-
-has_brew() { [[ -x `which brew` ]] }
-is_mac() { [[ $OSTYPE == darwin* ]] }
-is_freebsd() { [[ $OSTYPE == freebsd* ]] }
-is_linux() { [[ $OSTYPE == linux-gnu ]] }
+echo "Installing brew if you don't have it already…"
 
 if ! has_brew; then
-  echo "i don't have brew"
+  echo "you don't have brew!"
   if is_linux; then
-    echo "i am linux"
+    echo "you are on linux"
     sudo apt-get install build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev && ruby -e "$(wget -O- https://raw.github.com/Homebrew/linuxbrew/go/install)"
   fi
 
   if is_mac; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   fi
+else
+  echo "you already have brew!"
 fi
